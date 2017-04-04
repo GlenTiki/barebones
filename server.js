e5e72e37ae43f6a80b5e10688ca55d2a8fbbf8b3
Glen Keane <glenkeane.94@gmail.com>
2017-04-04 07:40:45 +0100

    added socker.io support

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 var express = require('express')
 var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
 app.use(express.static('output'))
 
-app.listen(3000, function () {
+io.on('connection', (socket) => {
+  console.log('a user has connected')
+
+  socket.on('chat message', function (msg) {
+    console.log('emitting', msg)
+    io.emit('chat message', msg)
+  })
+})
+
+http.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
--- src/Application.js
+++ src/Application.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './components/Header'
 import GenericComponent from './components/GenericComponent'
+import Chat from './components/Chat'
 
 export default class Application extends React.Component {
   render () {
@@ -8,6 +9,7 @@ export default class Application extends React.Component {
       <Header />
       <h1>Hello, ES6 and React 0.15!</h1>
       <GenericComponent />
+      <Chat />
       <p>
         barebones hmr test
       </p>
